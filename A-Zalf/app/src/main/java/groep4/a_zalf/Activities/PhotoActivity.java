e6dfa3cdfa4ce03357a7790ed3ebf1ff81b2401e
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-01-14 20:20:52 +0100

    new image and new activity

---
+++ A-Zalf/app/src/main/java/groep4/a_zalf/Activities/PhotoActivity.java
@@ -0,0 +1,15 @@
+package groep4.a_zalf.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import groep4.a_zalf.R;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+}
